81c31b933c090c694b6f86747ee0844ce5792693
blueblank <blueblank@gmail.com>
2013-06-20 09:03:10 -0400

    Bump version number to 0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ copyright = u'2013, Thrisp/Hurrata'
 # built documents.
 #
 # The short X.Y version.
-version = '0'
+version = '0.0.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
