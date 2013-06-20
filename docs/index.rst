d93301ece9fcb25b84800ce52c8f25be0f0ac71a
blueblank <blueblank@gmail.com>
2013-06-20 08:58:51 -0400

    building & automating release

--- docs/index.rst
+++ docs/index.rst
@@ -1,10 +1,11 @@
 Flask-Flarf
 ===========
 
-Configurable, template accessible request fiters for flask applications.
+Configurable, template accessible request filters for flask applications.
 
-Flask-Flarf allows you to set up usable and reusable per route filters which can
-be accessed in your views/controllers or templates. Use is general and limited to
-your needs and/or imagination.
+Flask-Flarf allows you to set up reusable per route filters accessible in your 
+views/controllers or templates. 
+
+Use is general and limited to your needs and/or imagination.
 
 .. include:: contents.rst.inc
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     license='MIT',
     author='Thrisp/Hurrata',
     author_email='blueblank@gmail.com',
-    description='Customize and filter Flask request',
+    description='Flask request filtering',
     long_description=__doc__,
     packages=['flask_flarf'],
     zip_safe=False,
@@ -29,6 +29,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
