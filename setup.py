889cb657f341ef8aeca21e5525fe9203f8c0404c
blueblank <blueblank@gmail.com>
2013-09-12 19:58:32 -0400

    Bump version number to 0.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Flarf',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/thrisp/flarf',
     license='MIT',
     author='Thrisp/Hurrata',
